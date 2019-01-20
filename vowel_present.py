17ad3089f18c323250725099b0598c0becd55123
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-01-20 11:21:12 +0800

    Create vowel_present.py

---
+++ vowel_present.py
@@ -0,0 +1,11 @@
+#this pgm is to find vowels in a string
+s=input()
+c=0
+for i in s:
+	if i=='a' or i=='e' or i=="i" or i=="o" or i=="u":
+		c=c+1
+		break
+if c==1:
+	print("yes")
+else:
+	print("no")
