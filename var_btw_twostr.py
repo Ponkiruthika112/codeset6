83f1066ae3412c0bbf037b8f78e45b1c54bea6b5
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-01-31 01:51:23 +0800

    Create var_btw_twostr.py

---
+++ var_btw_twostr.py
@@ -0,0 +1,21 @@
+# your code goes here
+a,b=map(str,input().split())
+p=a[0]
+
+for i in a:
+	a=a.replace(p,"1")
+	b=b.replace(p,"1")
+	if i!="1":
+		a=a.replace(i,"1")
+		b=b.replace(i,"1")
+for i in b:
+	if i!="1":
+		b=b.replace(i,"0")
+print(a,b)
+if abs(a.count("1")-b.count("1"))==1:
+	print("yes")
+else:
+	print("no")
+#did\f
+		
+
