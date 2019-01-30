497948615014cd69446e93694bf29cf736662124
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-01-31 01:28:20 +0800

    Update bet_prime.py

--- bet_prime.py
+++ bet_prime.py
@@ -1,10 +1,14 @@
 n,k=map(int,input().split())
+c=0
 for i in range(n,k+1):
 	if i==2:
-		print(i,end=" ")
+		c=c+1
 	for j in range(2,i):
 		if i%j==0:
 			break
 		if j==i-1 and i%j!=0:
-			print(i,end=" ")
+			c=c+1
+print(c)
+	
 	#between the gn nos find out prime nos
+#thisn
