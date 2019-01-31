f08a31e9948b057e6616088327c5efad5fb42522
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-01-31 23:25:39 +0800

    Update div.py

--- div.py
+++ div.py
@@ -1,8 +1,10 @@
+def gcd(a,b):
+	if b==0:
+		return a
+	else:
+		return gcd(b,a%b)
+
 l,r=map(int,input().split())
-if r>l:
-	l,r=r,l
-if l%r==0:
-	print(l)
-else:
-	print(l*r)
-#for smallest div value
+k=gcd(l,r)
+print(l*r//k)
+#smallest divisible
