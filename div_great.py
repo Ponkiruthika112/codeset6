7d1a820122044f2079bef9e006602779650fcf2d
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-02 14:47:43 +0800

    Update div_great.py

--- div_great.py
+++ div_great.py
@@ -1,8 +1,10 @@
- def gcd(a,b):
-    if b==0:
-        return a
-    else:
-        return gcd(b,a%b)
+
+def gcd(a,b):
+	if b==0:
+		return a
+ else:
+  return gcd(b,a%b)
 n,k=map(int,input().split())
 print(gcd(n,k))
 #this is for to find the greatest no divide by both
+
