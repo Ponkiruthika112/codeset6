0a743e15cc8e6c5bf5d848c533fd48750056fe7b
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-02 14:27:50 +0800

    Update points.py

--- points.py
+++ points.py
@@ -8,7 +8,6 @@ x3,y3=map(int,input().split())
 p1=colinear(x1,y1,x2,y2)
 p2=colinear(x2,y2,x3,y3)
 p3=colinear(x3,y3,x1,y1)
-print(p1,p2,p3)
 if p3==p1+p2 or p1==p2+p3 or p2==p1+p3:
     print("yes")
 else:
