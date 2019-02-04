b49c31cc1d663e97fc5d83af9a0f60b7adcc974b
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-04 16:00:30 +0530

    Update sum_3_7.py

--- sum_3_7.py
+++ sum_3_7.py
@@ -3,4 +3,17 @@ n=int(input())
 if n%3==0 or n%7==0:
 	print("yes")
 else:
-	print("no")
+	k=n%3
+	y=n-k
+	i=0
+	while i<y:
+		s=y-3
+		if s+7==n:
+			print("yes")
+			break
+		else:
+			y=s
+			i=i+1
+	else:
+		print("no")
+#for sum of 3 and 7
