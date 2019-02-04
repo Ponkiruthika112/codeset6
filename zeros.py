327cfe1eb58a76e1ca9db0d9c0e98dd6ea065eae
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-04 21:10:17 +0800

    Create zeros.py

---
+++ zeros.py
@@ -0,0 +1,16 @@
+# your code goes here
+l=list(map(int,input().split()))
+k=l.count(0)
+i=0
+s=""
+while i<len(l):
+    if l[i]==0:
+        l.remove(l[i])
+    else:
+        i=i+1
+for i in range(0,k):
+    l.append(0)
+for i in range(0,len(l)):
+    s=s+str(l[i])+" "
+print(s.strip())
+#for append zeros at end
