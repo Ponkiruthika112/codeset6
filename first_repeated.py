55da10f37d736997b3968d7b2703d713d06b255d
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-06 15:34:25 +0800

    Update first_repeated.py

--- first_repeated.py
+++ first_repeated.py
@@ -5,11 +5,14 @@ s=""
 for i in range(0,n):
         if l.count(l[i])>1:
                 p.append(l[i])
-k=[]
-for i in range(0,len(p)-1):
-        for j in range(i+1,len(p)):
-                if p[i]==p[j]:
-                        k.append([j-i,p[i]])
-k.sort()
-print(k[0][1])
+if len(p)!=0:
+        k=[]
+        for i in range(0,len(p)-1):
+                for j in range(i+1,len(p)):
+                        if p[i]==p[j]:
+                                k.append([j-i,p[i]])
+        k.sort()
+        print(k[0][1])
+else:
+        print("unique")
 #repeated one
