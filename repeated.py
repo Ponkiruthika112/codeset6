c47e1407b0c473106759c943eecf2eb9028d29b9
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-06 14:04:02 +0800

    Update repeated.py

--- repeated.py
+++ repeated.py
@@ -5,15 +5,19 @@ s=""
 for i in range(0,n):
         if l.count(l[i])>1:
                 p.append(l[i])
-p.sort()
-i=0
-while i<len(p)-1:
-        if p[i]==p[i+1]:
-                p.remove(p[i])
-        else:
-                i=i+1
-for i in range(0,len(p)):
-        s=s+str(p[i])+" "
-print(s.strip())
-#repeated nos        
+if len(p)!=0:
+        
+        p.sort()
+        i=0
+        while i<len(p)-1:
+                if p[i]==p[i+1]:
+                        p.remove(p[i])
+                else:
+                        i=i+1
+        for i in range(0,len(p)):
+                s=s+str(p[i])+" "
+        print(s.strip())
+else:
+        print("unique")
 
+#nos
