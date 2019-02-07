4e0dd2900cb910f4523f342d948b550341d7b899
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-07 10:41:43 +0800

    Update sum_list.py

--- sum_list.py
+++ sum_list.py
@@ -4,6 +4,12 @@ for i in range(0,n):
         for j in range(i+1,n):
                 if l[i]+l[j]==0:
                         print(l[i],l[j])
-
+                elif l[i]<0 or l[j]<0:
+                        if l[i]+l[j]==-1 or l[i]+l[j]==1:
+                                print(l[i],l[j])
+                                break
+                        
+                
                                 
-#to find the nos which we add to get 0
+               
+#for find nos
