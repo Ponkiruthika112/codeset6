3e0b6acf2599d07120f5ec8d16e60faf3ea81ba4
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-09 14:56:08 +0800

    Update odd_factors.py

--- odd_factors.py
+++ odd_factors.py
@@ -1,7 +1,9 @@
 n=int(input())
+s=""
 for i in range(1,n):
     if n%i==0 and i%2!=0:
-        print(i,end=" ")
+        s=s+str(i)+" "
 if i==n-1 and n%2!=0:
-    print(i+1)
+    s=s+str(i)+" "
+print(s.strip())
 #odd factors
