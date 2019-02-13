a595d00ad4804dd113ee6d81d2da37dace551816
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-13 23:27:13 +0800

    Update partition.py

--- partition.py
+++ partition.py
@@ -4,10 +4,12 @@ y=n//b
 c=0
 for i in range(0,x):
     for j in range(y,0,-1):
-        if a*i+b*j==n and (i+j)%2==0:
+        if a*i+b*j==n and (i+j)%2==0 and i==j:
             c=c+1
 if c==0:
     print("NO")
 else:
     print("YES")
-#partition
+
+        
+#PK
